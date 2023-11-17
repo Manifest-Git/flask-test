1002df4ec4512d85eba08014c400059de25a34cb
Jake Johnson <jakejohnson3700@gmail.com>
2023-11-17 16:21:53 +0300

    change port

--- main.py
+++ main.py
@@ -66,6 +66,6 @@ def checkLogin(username, password):
     driver.quit()
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8989)
 
 
